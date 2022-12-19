5d392a704929a6f7885b61fcd508cea15e1ab5fc
akki1821 <pppatelakash@gmail.com>
2022-12-19 19:16:08 +0530

    Update Course.js with new features

--- src/Course.js
+++ src/Course.js
@@ -4,10 +4,12 @@ import {Header,Content} from './Header';
 
 const Course = (props) => {
   const { course } = props;
+  const totalExercises = course.parts.reduce((acc, part) => acc + part.exercises, 0)
   return (
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <p>Total exercises: {totalExercises}</p>
     </div>
   );
 };
